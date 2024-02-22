2797ba736f10ec04e3297f71035c78517a963fc3
TideDra <gearyzhang@outlook.com>
2024-02-22 14:27:35 +0000

    use sgl model as entity extractor

--- src/Woodpecker/models/entity_extractor.py
+++ src/Woodpecker/models/entity_extractor.py
@@ -1,50 +1,24 @@
 from typing import  Dict
-from tqdm import tqdm
-import openai
-import time
+from sglang import function,user,assistant,system,gen
 
-NUM_SECONDS_TO_SLEEP = 0.5
-PROMPT_TEMPLATE='''Given a sentence, extract the entities within the sentence for me. 
+@function
+def extractor(s,sentence):
+    s += system('''You are an AI assistant that extracts the entities within the given sentence. 
 Extract the common objects and summarize them as general categories without repetition, merge essentially similar objects.
 Avoid extracting abstract or non-specific entities, such as lines, paintings, and so on. Make sure the entities can be detected by an object detection model.
-Extract entity in the singular form. Output all the extracted types of items in one line and separate each object type with a period. If there is nothing to output, then output a single "None". Notice again that the output should be in the singular form.
-
-Examples:
-Sentence:
-The image depicts a man laying on the ground next to a motorcycle, which appears to have been involved in a crash.
-
-Output:
-man.motorcycle
-
-Sentence:
-There are a few people around, including one person standing close to the motorcyclist and another person further away.
-
-Output:
-person.motorcyclist
-
-Sentence:
-No, there is no car in the image.
-
-Output:
-car
-
-Sentence:
-The image depicts a group of animals, with a black dog, a white kitten, and gray cats, sitting on a bed.
-
-Output:
-dog.cat.bed
-
-Sentence:
-{sentence}
-
-Output:'''
-
+Extract entity in the singular form. Output all the extracted types of items in one line and separate each object type with a period. If there is nothing to output, then output a single "None". Notice again that the output should be in the singular form.''')
+    s += user("Sentence:\nThe image depicts a man laying on the ground next to a motorcycle, which appears to have been involved in a crash.")
+    s += assistant("Output:\nman.motorcycle")
+    s += user("Sentence:\nThere are a few people around, including one person standing close to the motorcyclist and another person further away.")
+    s += assistant("Output:\nperson.motorcyclist")
+    s += user("Sentence:\nNo, there is no car in the image.")
+    s += assistant("Output:\ncar")
+    s += user("Sentence:\nThe image depicts a group of animals, with a black dog, a white kitten, and gray cats, sitting on a bed.")
+    s += assistant("Output:\ndog.cat.bed")
+    s += user(f"Sentence:\n{sentence}")
+    s += assistant("Output:\n"+gen('entities'))
 
 class EntityExtractor:
-    def __init__(self, chatbot):
-
-        self.chatbot = chatbot
-        
     
     def extract_entity(self, sample: Dict):
         extracted_entities = []
@@ -52,14 +26,8 @@ class EntityExtractor:
             entity_str = self.get_res(sent)
             extracted_entities.append(entity_str.strip('.'))
         sample['named_entity'] = extracted_entities
-        
         return sample
 
-    def get_res(self,sent: str, max_tokens: int=1024,model='gpt-3.5-turbo'):
-        content = PROMPT_TEMPLATE.format(sentence=sent)
-
-        system =  'You are a language assistant that helps to rewrite a passage according to instructions.'
-
-        response = self.chatbot.complete(content,system_message = system)
-
-        return response
+    def get_res(self,sent: str,):
+        state = extractor.run({'sentence':sent},temperature=0,max_new_tokens=1024)
+        return state['entities'].strip()
