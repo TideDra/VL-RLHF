26e036d2d3ab3aa309127b458288cd258b8c09f3
TideDra <gearyzhang@outlook.com>
2024-02-22 14:27:23 +0000

    use sgl model as preprocessor

--- src/Woodpecker/models/preprocessor.py
+++ src/Woodpecker/models/preprocessor.py
@@ -1,40 +1,27 @@
 from typing import  Dict
 import spacy
-import openai
-import time
-from tqdm import tqdm
 import os
-NUM_SECONDS_TO_SLEEP = 0.5
+from sglang import function,user,assistant,system,gen
 
-PROMPT_TEMPLATE='''Given a passage, you are required to replace pronouns such as "they" with the actual entities they refer to based on the context, then output the passage after replacement.
+@function
+def rewriter(s,text):
+    s += system('''You are an AI assistant that helps to rewrite given passages.
+Given a passage, you are required to replace the subject in each sentence which is pronoun such as "they" with the actual entities they refer to based on the context, so that they can be uniquely refered to without the previous context. Then output the passage after replacement.
 Only replace the pronouns if there is any, and do not change anything else in the original passage. 
 If there is nothing to replace, then keep the original sentences unchanged, do not add any new sentence.
-The modification should be as small as possible, and the output passage should have the same number of sentences as the original passage.
-
-Examples:
-Passage:
-The image depicts a kitchen scene, with a man wearing a t-shirt, a pair of shorts, and a baseball cap standing in the middle of it. He appears to be smoking.
-
-Rewritten passage:
-The image depicts a kitchen scene, with a man wearing a t-shirt, a pair of shorts, and a baseball cap standing in the middle of it. The man appears to be smoking.
-
-Passage:
-The image depicts a group of animals, with a black dog, a white kitten, and a gray cat, sitting on a bed. The dog is in the center of the group, while the cats are positioned to the left and right. 
-
-Rewritten passage:
-The image depicts a group of animals, with a black dog, a white kitten, and a gray cat, sitting on a bed. The dog is in the center of the group, while the cats are positioned to the left and right. 
-
-
-Passage:
-{text}
-
-Rewritten passage:'''
-
+The modification should be as small as possible, and the output passage should have the same number of sentences as the original passage.''')
+    s += user("Passage:\nThe image depicts a kitchen scene, with a man wearing a t-shirt, a pair of shorts, and a baseball cap standing in the middle of it. He appears to be smoking.")
+    s += assistant("Rewritten passage:\nThe image depicts a kitchen scene, with a man wearing a t-shirt, a pair of shorts, and a baseball cap standing in the middle of it. The man appears to be smoking.")
+    s += user("Passage:\nThe image features a single water bottle left on a green wooden bench. The bench has a yellow armrest and is situated on a sidewalk. The surrounding area appears to be a park, as there are trees visible in the background.\n\nThe water bottle, which is almost empty, is placed near the middle of the bench. It is possible that someone left the bottle behind or forgot to take it with them after enjoying a break in the park. The scene conveys a sense of neglect or carelessness, as the empty water bottle sits unattended on the park bench.")
+    s += assistant("Rewritten passage:\nThe image features a single water bottle left on a green wooden bench. The bench has a yellow armrest and is situated on a sidewalk. The surrounding area appears to be a park, as there are trees visible in the background.\n\nThe water bottle, which is almost empty, is placed near the middle of the bench. It is possible that someone left the bottle behind or forgot to take it with them after enjoying a break in the park. The scene conveys a sense of neglect or carelessness, as the empty water bottle sits unattended on the park bench.")
+    s += user("Passage:\nThe image features a person wearing snow skis, performing a trick on a small ramp or wooden obstacle in the snow. The person is in the air, having launched off the ramp, and appears to be having fun. \n\nThere are several other people in the scene, some of whom are also on skis. However, they are located further away from the main action and are not actively involved in the trick being performed. The snow covers the ground, creating a winter sports environment for the skiers.")
+    s += assistant("Rewritten passage:\nThe image features a person wearing snow skis, performing a trick on a small ramp or wooden obstacle in the snow. The person is in the air, having launched off the ramp, and appears to be having fun. \n\nThere are several other people in the scene, some of whom are also on skis. However, the people other than the person in the air are located further away from the main action and are not actively involved in the trick being performed. The snow covers the ground, creating a winter sports environment for the skiers.")
+    s += user(f"Passage:\n{text}")
+    s += assistant("Rewritten passage:\n"+gen('rewrite'))
 
 class PreProcessor:
     
-    def __init__(self, chatbot):
-        self.chatbot = chatbot
+    def __init__(self):
         try:
             self.nlp = spacy.load('en_core_web_lg')
         except:
@@ -59,10 +46,7 @@ class PreProcessor:
         sample['orig_split_sents'] = orig_split_sents
         return sample
 
-    def get_output(self,text: str, max_tokens: int=1024,model='gpt-3.5-turbo'):
-        content = PROMPT_TEMPLATE.format(text=text)
+    def get_output(self,text: str):
+        state = rewriter.run({'text':text},temperature=0,max_new_tokens=1024)
 
-        system = 'You are a language assistant that helps to rewrite a passage according to instructions.'
-        response = self.chatbot.complete(content,system_message = system)
-
-        return response
+        return state['rewrite']
